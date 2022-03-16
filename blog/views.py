185e4b2576d53aec379c1fa1585a0d805f753000
Kat632 <katherine.biggs@mail.com>
2022-03-16 09:14:00 +0000

    add alert messages

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
+from django.contrib import messages
 from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import CommentForm
@@ -50,6 +51,8 @@ class PostDetail(View):
             comment = comment_form.save(commit=False)
             comment.post = post
             comment.save()
+            messages.add_message(request, messages.INFO,
+                                'Your comment was added successfully.')
         else:
             comment_form = CommentForm()
 
